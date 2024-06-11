7b6bcba22aea2dc7f78cb9433b63ecebe769947a
matheusbanqueiro <mbanqueirolima@gmail.com>
2024-06-11 08:52:53 -0300

    first commit

--- components/aboutSection.js
+++ components/aboutSection.js
@@ -4,12 +4,12 @@ import React from 'react';
 
 const AboutSection = ({ title, subtitle }) => {
   return (
-    <div className="py-12 bg-gray-100 text-center">
-      <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
-        <h2 className="text-3xl font-bold text-gray-900">
+    <div className="py-12 text-center flex justify-center bg-white3">
+      <div className="max-w-4xl">
+        <h2 className="text-6xl font-normal text-maindark">
           {title}
         </h2>
-        <p className="mt-4 text-lg text-gray-700">
+        <p className="mt-4 text-lg text-maindark">
           {subtitle}
         </p>
       </div>
