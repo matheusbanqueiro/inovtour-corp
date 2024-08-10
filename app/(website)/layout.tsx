f5573c4bcb1aa6756222e022d589119858c4cddd
Govinda <govinda777@gmail.com>
2024-08-10 13:33:26 -0300

    Fix error to loading pixel

--- app/(website)/layout.tsx
+++ app/(website)/layout.tsx
@@ -3,8 +3,9 @@ import { getSettings } from "@/lib/sanity/client";
 import Footer from "@/components/footer";
 import { urlForImage } from "@/lib/sanity/image";
 import Navbar from "@/components/navbar";
+import Script from 'next/script';
 
-// Metadata é gerado de forma síncrona
+// Função para gerar metadados de forma síncrona
 export async function generateMetadata(): Promise<Metadata> {
   const settings = await getSettings();
 
@@ -17,8 +18,7 @@ export async function generateMetadata(): Promise<Metadata> {
     keywords: ["travel", "tourism", "vacation", "trip"],
     authors: [{ name: "Govinda Systems DAO" }],
     alternates: {
-      canonical: settings?.url || "https://default-url.com", // Definindo a URL canônica
-      // Você pode adicionar outras URLs alternativas aqui, se necessário
+      canonical: settings?.url || "https://default-url.com",
     },
     openGraph: {
       images: [
@@ -40,16 +40,51 @@ export async function generateMetadata(): Promise<Metadata> {
   };
 }
 
-// Função Layout não deve ser async
+// Layout principal do website
 export default async function Layout({ children }: { children: React.ReactNode }) {
-  const settings = await getSettings(); // Resolva a Promise aqui
+  const settingsPromise = getSettings(); // Obtém a promessa
+  const settings = await settingsPromise; // Resolve a promessa antes de usar
 
   return (
-    <div id="__next">
-      <Navbar {...settings} />
-      <div>{children}</div>
-      <Footer {...settings} />
-    </div>
+    <html lang="en">
+      <head>
+        <Script
+          id="fb-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window,document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '${process.env.NEXT_PUBLIC_PIXEL_ID}');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src={`https://www.facebook.com/tr?id=${process.env.NEXT_PUBLIC_PIXEL_ID}&ev=PageView&noscript=1`}
+            alt="Facebook Pixel"
+          />
+        </noscript>
+      </head>
+      <body>
+        <div id="__next">
+          <Navbar {...settings} /> {/* Usando os dados resolvidos */}
+          <div>{children}</div>
+          <Footer {...settings} /> {/* Usando os dados resolvidos */}
+        </div>
+      </body>
+    </html>
   );
 }
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "@/styles/tailwind.css";
 import { Providers } from "./providers";
 import { cx } from "@/utils/all";
 import { Inter, Lora } from "next/font/google";
+import Script from "next/script"; // Certifique-se de manter esta importação
 
 const inter = Inter({
   subsets: ["latin"],
@@ -24,6 +25,35 @@ export default function RootLayout({
       suppressHydrationWarning
       className={cx(inter.variable, lora.variable)}>
       <body className="antialiased text-gray-800 dark:bg-black dark:text-gray-400">
+        {/* Adicione o Script aqui, dentro do body */}
+        <Script
+          id="fb-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window,document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '${process.env.NEXT_PUBLIC_PIXEL_ID}');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src={`https://www.facebook.com/tr?id=${process.env.NEXT_PUBLIC_PIXEL_ID}&ev=PageView&noscript=1`}
+            alt="Facebook Pixel"
+          />
+        </noscript>
         <Providers>{children}</Providers>
       </body>
     </html>
